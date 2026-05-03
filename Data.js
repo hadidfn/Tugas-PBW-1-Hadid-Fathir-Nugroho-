91abd85d9488a2d6d60b5547604c9fc831d8405a
hadidfn <hadidfn2004@gmail.com>
2026-05-04 01:26:17 +0700

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,27 @@
+const dataBahanAjar = [
+    { kode: "MSIM4301", nama: "Sistem Informasi Manajemen", stok: 67 },
+    { kode: "MKWN4110", nama: "Pancasila", stok: 67 }
+];
+
+const dataTracking = {
+    "DO12345": {
+        nama: "Hadid Fathir Nugroho",
+        prodi: "Sistem Informasi",
+        status: "Dalam Perjalanan",
+        progress: 70,
+        ekspedisi: "POS INDONESIA",
+        tanggal: "2026-05-02",
+        paket: "Bahan Ajar Cetak",
+        total: "Rp 2.100.000"
+    },
+    "DO23456": {
+        nama: "Dimas Prasetyo",
+        prodi: "Akutansi",
+        status: "Selesai",
+        progress: 100,
+        ekspedisi: "JNE Cargo",
+        tanggal: "2026-04-28",
+        paket: "Bahan Ajar Cetak",
+        total: "Rp 1.850.000"
+    }
+};
